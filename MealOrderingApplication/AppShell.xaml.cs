bbadc30dba656aed41eec01278fd4052f408e5c4
Rishabh Atul Mishra <mishrris@shernet.sheridancollege.ca>
2024-07-28 22:56:37 -0400

    Add project files.

---
+++ MealOrderingApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MealOrderingApplication
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
